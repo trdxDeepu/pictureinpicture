791a3be084646c257dccd95a94353337c317b8d2
trdxlucifer <singhdeepu566@gmail.com>
2022-06-06 22:49:13 +0530

    added try and catch and mediastreams

--- app.js
+++ app.js
@@ -6,9 +6,13 @@ const button = document.getElementById('button');
 async function selectMediaStream( ) {
     try {
 
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
     }
     catch(error){
 
-        
+        console.log('whoops, error here:',error)
+
     }
 }
+
+selectMediaStream();
