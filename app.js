84e57371eea84c87c9bd7272019a4ac663215e85
trdxlucifer <singhdeepu566@gmail.com>
2022-06-06 22:45:37 +0530

    make a asnc functtion

---
+++ app.js
@@ -0,0 +1,14 @@
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+//promt to select media stream and pass to video and then play
+
+async function selectMediaStream( ) {
+    try {
+
+    }
+    catch(error){
+
+        
+    }
+}
